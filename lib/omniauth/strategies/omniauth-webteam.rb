53f3724622c5cce7344c94d124ebedbfbc4813a4
AchilleAsh <achille.ash@gmail.com>
2015-09-21 23:49:08 +0200

    Initial ruby files

---
+++ lib/omniauth/strategies/omniauth-webteam.rb
@@ -0,0 +1,57 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Webteam < OmniAuth::Strategies::OAuth2
+      DEFAULT_SCOPE = 'user'
+      # Give your strategy a name.
+      option :name, "webteam_ensea"
+
+      # This is where you pass the options you would pass when
+      # initializing your consumer from the OAuth gem.
+      option :client_options, {
+        :site => "https://webteam.ensea.fr/api",
+       :authorize_url => 'https://webteam.ensea.fr/oauth/v2/auth',
+    :token_url => 'https://webteam.ensea.fr/oauth/v2/token'
+  }
+
+      # These are called after authentication has succeeded. If
+      # possible, you should try to set the UID without making
+      # additional calls (if the user id is returned with the token
+      # or as a URI parameter). This may not be possible with all
+      # providers.
+      uid{ raw_info['id'] }
+
+      info do
+        {
+          :first_name => raw_info['first_name'],
+          :last_name => raw_info['last_name'],
+          :name => raw_info['first_name'] + ' ' + raw_info['last_name'],
+          :email => raw_info['email'],
+          :nickname => raw_info['username']
+        }
+      end
+
+      extra do
+        {
+          'raw_info' => raw_info
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/user').parsed
+      end
+
+        def request_phase
+        options[:authorize_params] = {
+          :client_id     => options['client_id'],
+          :response_type => 'code',
+          :scopes        => (options['scope'] || DEFAULT_SCOPE)
+        }
+
+        super
+      end
+      
+    end
+  end
+end
---
+++ omniauth-webteam.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'Omniauth Webteam'
+  s.version     = '0.0.0'
+  s.date        = '2015-09-21'
+  s.summary     = "Webteam ENSEA Auth"
+  s.description = "Oauth2 client for Webteam ENSEA"
+  s.authors     = ["Timothé Perez"]
+  s.email       = 'webteam@ensea.fr'
+  s.files       = ["lib/omniauth-webteam.rb"]
+  s.homepage    =
+    'https://webteam.ensea.fr'
+  s.license       = 'MIT'
+end
